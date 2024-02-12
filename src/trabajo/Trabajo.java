7782ca7bd96047a35ee8ad12ec82b6c4c75b499b
Varelaso32 <samyst2013@hotmail.com>
2024-02-11 19:21:46 -0500

    Se agrega el Proyecto

---
+++ src/trabajo/Trabajo.java
@@ -0,0 +1,19 @@
+
+package trabajo;
+
+/**
+ *
+ * @author      Samuel Varela 
+ *         Juan Manuel De La Cruz
+ *           Daniel Felipe Cleves
+ */
+public class Trabajo {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
